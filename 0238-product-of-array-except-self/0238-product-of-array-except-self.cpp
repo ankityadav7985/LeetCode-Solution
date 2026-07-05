ae8fea0dc89425bf890384cf42f06d156ee19c61
ankityadav7985 <2k23.cs2314003@gmail.com>
2026-07-05 23:15:39 +0530

    Time: 3 ms (37.74%), Space: 40.3 MB (60.84%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> v(n);
-
-        int product = 1;
-        int zeroCount = 0;
-
-        // Calculate product of non-zero elements and count zeros
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0)
-                zeroCount++;
-            else
-                product *= nums[i];
+        int n=nums.size();
+        vector<int>v(n,0);
+        int sum=1;
+        int zero=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0)
+            sum*=nums[i];
+            
+            else zero++;
         }
-
-        // Fill answer
-        for (int i = 0; i < n; i++) {
-            if (zeroCount > 1)
-                v[i] = 0;
-            else if (zeroCount == 1) {
-                if (nums[i] == 0)
-                    v[i] = product;
+        for(int i=0;i<nums.size();i++){
+            if(zero>1)
+                v[i]=0;
+            
+                else if(zero==1){
+                if(nums[i]==0)v[i]=sum;
                 else
-                    v[i] = 0;
-            }
-            else {
-                v[i] = product / nums[i];
-            }
-        }
+                v[i]=0;
+                }
 
+            else
+            v[i]=sum/nums[i];
+
+        }
         return v;
     }
 };
