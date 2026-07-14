231e765070aec6dfdea593a9390f79363cb28802
ankityadav7985 <2k23.cs2314003@gmail.com>
2026-07-14 22:05:03 +0530

    Time: 7 ms (0.4%), Space: 11 MB (1.79%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
+int ans=0;
 int solve(int num1,int num2,int count){
 if(num1<=0||num2<=0)return count;
 if(num1>=num2){
-     return solve(num1-num2,num2,count+1);
+     ans=solve(num1-num2,num2,count+1);
 }
-    return solve(num1,num2-num1,count+1);
-
+else
+    ans=solve(num1,num2-num1,count+1);
+return ans;
 }
     int countOperations(int num1, int num2) {
         return solve(num1,num2,0); 
