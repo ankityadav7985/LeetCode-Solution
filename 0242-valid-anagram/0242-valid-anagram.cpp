2f832c206327b98c2b0afac92eef29769064dd77
ankityadav7985 <2k23.cs2314003@gmail.com>
2026-07-16 21:53:01 +0530

    Time: 4 ms (34.71%), Space: 9.6 MB (76.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-         if(s.length()!=t.length())return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+            if(s.length()!=t.length())
+        return false;
+
+        int freq[26];
+        for(char c:s){
+            freq[c-'a']++;
+        }
+          for(char c:t){
+            freq[c-'a']--;
+        }
+        
+        for(int i=0;i<26;i++)
+    {
+        if(freq[i]!=0)
+            return false;
+    }
+        return true;
         
     }
 };
